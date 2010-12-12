1a3530520fc08408d7dc89b85d99b1cd6a8585f8
sathish <sahthish316@gmail.com>
2010-12-12 16:52:12 +0530

    Adding title to home, contact, about pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 
 describe PagesController do
 
+  render_views
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    
+    it "should have title" do
+      get 'home'
+      response.should have_selector("title", :content => "Rails 3 Tutorial | Home")
     end
   end
 
@@ -14,12 +21,22 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have title" do
+      get 'contact'
+      response.should have_selector('title', :content => "Rails 3 Tutorial | Contact")
+    end
   end
 
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+    
+    it "should have title" do
+      get 'about'
+      response.should have_selector('title', :content => 'Rails 3 Tutorial | About')
     end
   end
 
